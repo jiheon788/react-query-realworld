b128777a5c85eb8ff6b62188af359633946b5c82
jiheon788 <jiheon788@ajou.ac.kr>
2023-02-08 00:19:00 +0900

    feat: global feeds

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,33 +1,42 @@
+import { convertToDate } from '@/lib/utils';
 import { getArticles } from '@/repositories/articles/ArticlesRepository';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Feed = () => {
+  const [articles, setArticles] = useState([]);
+
   useEffect(() => {
-    getArticles({ limit: 10 });
+    getArticles({ limit: 10 }).then((res) => {
+      setArticles(res.data.articles);
+    });
   }, []);
 
   return (
-    <div className="article-preview">
-      <div className="article-meta">
-        <a href="profile.html">
-          <img src="http://i.imgur.com/Qr71crq.jpg" alt="profile" />
-        </a>
-        <div className="info">
-          <a href="/" className="author">
-            Eric Simons
+    <>
+      {articles.map((article: any) => (
+        <div className="article-preview" key={article.title}>
+          <div className="article-meta">
+            <a href="profile.html">
+              <img src={article.author.image} alt="profile" />
+            </a>
+            <div className="info">
+              <a href="/" className="author">
+                {article.author.username}
+              </a>
+              <span className="date">{convertToDate(article.createdAt)}</span>
+            </div>
+            <button className="btn btn-outline-primary btn-sm pull-xs-right">
+              <i className="ion-heart"></i> {article.favoritesCount}
+            </button>
+          </div>
+          <a href="/" className="preview-link">
+            <h1>{article.title}</h1>
+            <p>{article.description}</p>
+            <span>Read more...</span>
           </a>
-          <span className="date">January 20th</span>
         </div>
-        <button className="btn btn-outline-primary btn-sm pull-xs-right">
-          <i className="ion-heart"></i> 29
-        </button>
-      </div>
-      <a href="/" className="preview-link">
-        <h1>How to build webapps that scale</h1>
-        <p>This is the description for the post.</p>
-        <span>Read more...</span>
-      </a>
-    </div>
+      ))}
+    </>
   );
 };
 
---
+++ src/lib/utils.ts
@@ -0,0 +1,19 @@
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const convertToDate = (datetime: string) => {
+  const [year, month, day] = datetime.split('T')[0].split('-').map(Number);
+  return `${months[month - 1]} ${day}, ${year}`;
+};
