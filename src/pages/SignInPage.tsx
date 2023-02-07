f0b52dee9f75af4758984410f2bf853f2d93e29f
jiheon788 <jiheon788@ajou.ac.kr>
2023-02-07 22:43:45 +0900

    feat: signin

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,5 +1,69 @@
+import useInputs from '@/lib/hooks/useInputs';
+import routerMeta from '@/lib/routerMeta';
+import token from '@/lib/token';
+import { postLogin } from '@/repositories/users/usersRepository';
+import { Link, useNavigate } from 'react-router-dom';
+
 const SignInPage = () => {
-  return <>Auth</>;
+  const [signIndata, onChangeSignInData] = useInputs({ email: '', password: '' });
+  const navigate = useNavigate();
+
+  const onLogin = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    postLogin({ user: signIndata })
+      .then((res) => {
+        token.setToken('jwtToken', res.data.user.token);
+        navigate('/');
+      })
+      .catch((err) => {
+        alert(JSON.stringify(err.response.data.errors || 'error'));
+      });
+  };
+
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign up</h1>
+            <p className="text-xs-center">
+              <Link to={routerMeta.SignUpPage.path}>Not registered?</Link>
+            </p>
+
+            {/* <ul className="error-messages">
+            <li>That email is already taken</li>
+          </ul> */}
+
+            <form onSubmit={onLogin}>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={signIndata.email}
+                  onChange={onChangeSignInData}
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={signIndata.password}
+                  onChange={onChangeSignInData}
+                />
+              </fieldset>
+              <button type="submit" className="btn btn-lg btn-primary pull-xs-right">
+                Sign up
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignInPage;
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -21,60 +21,58 @@ const SignUpPage = () => {
   };
 
   return (
-    <>
-      <div className="auth-page">
-        <div className="container page">
-          <div className="row">
-            <div className="col-md-6 offset-md-3 col-xs-12">
-              <h1 className="text-xs-center">Sign up</h1>
-              <p className="text-xs-center">
-                <Link to={routerMeta.SignInPage.path}>Have an account?</Link>
-              </p>
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign up</h1>
+            <p className="text-xs-center">
+              <Link to={routerMeta.SignInPage.path}>Have an account?</Link>
+            </p>
 
-              {/* <ul className="error-messages">
+            {/* <ul className="error-messages">
                 <li>That email is already taken</li>
               </ul> */}
 
-              <form onSubmit={onRegister}>
-                <fieldset className="form-group">
-                  <input
-                    className="form-control form-control-lg"
-                    type="text"
-                    placeholder="Your Name"
-                    name="username"
-                    value={signUpdata.username}
-                    onChange={onChangeSignUpData}
-                  />
-                </fieldset>
-                <fieldset className="form-group">
-                  <input
-                    className="form-control form-control-lg"
-                    type="email"
-                    placeholder="Email"
-                    name="email"
-                    value={signUpdata.email}
-                    onChange={onChangeSignUpData}
-                  />
-                </fieldset>
-                <fieldset className="form-group">
-                  <input
-                    className="form-control form-control-lg"
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    value={signUpdata.password}
-                    onChange={onChangeSignUpData}
-                  />
-                </fieldset>
-                <button type="submit" className="btn btn-lg btn-primary pull-xs-right">
-                  Sign up
-                </button>
-              </form>
-            </div>
+            <form onSubmit={onRegister}>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="text"
+                  placeholder="Your Name"
+                  name="username"
+                  value={signUpdata.username}
+                  onChange={onChangeSignUpData}
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={signUpdata.email}
+                  onChange={onChangeSignUpData}
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={signUpdata.password}
+                  onChange={onChangeSignUpData}
+                />
+              </fieldset>
+              <button type="submit" className="btn btn-lg btn-primary pull-xs-right">
+                Sign up
+              </button>
+            </form>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
