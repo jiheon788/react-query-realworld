0a633f457ae64568d83c00e49a61c93d01a88f7c
jiheon788 <jiheon788@ajou.ac.kr>
2023-02-17 23:55:04 +0900

    style: correct typos and remove legacy

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -31,7 +31,7 @@ const SignInPage = () => {
       <div className="container page">
         <div className="row">
           <div className="col-md-6 offset-md-3 col-xs-12">
-            <h1 className="text-xs-center">Sign up</h1>
+            <h1 className="text-xs-center">Sign in</h1>
             <p className="text-xs-center">
               <Link to={routerMeta.SignUpPage.path}>Not registered?</Link>
             </p>
