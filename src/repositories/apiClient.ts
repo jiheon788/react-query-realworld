c64962cad0d4e44af21c52b9ff8e420e7606b0ee
Mariano Agustin Nespral <marianoagustinnespral@Marianos-MacBook-Pro.local>
2026-07-20 14:23:41 -0300

    refactor: set the api to localhost:3000

--- src/repositories/apiClient.ts
+++ src/repositories/apiClient.ts
@@ -2,7 +2,7 @@ import { ACCESS_TOKEN_KEY } from '@/constants/token.contant';
 import token from '@/lib/token';
 import axios, { AxiosResponse, InternalAxiosRequestConfig, AxiosError } from 'axios';
 
-const host = 'https://api.realworld.io/api';
+const host = 'http://localhost:3000/api';
 
 const apiClient = axios.create({
   baseURL: host,
